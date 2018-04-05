e9f802ca6feb4d45b04cb17de019b198a215a350
DarrienG <darrienglasser@outlook.com>
2018-04-05 15:33:32 -0400

    Clean up code, fix smol issues

--- pt.py
+++ pt.py
@@ -2,13 +2,16 @@
 
 import requests
 import sys
-import os
+
+cmd_usage = "Command usage\n\t-b \"<TWEET>\" to post to Bill\n\t-j \"<TWEET>\" to post to Jay\n\t-h for help" 
 
 POST_TWEET_LINK = ""
 SECRET_KEY = ""
 
 if len(sys.argv) < 2:
-    raise Exception("Need -b for Bill or -j for Jay and tweet text in a string afterwards")
+    print("Not enough arguments")
+    print(cmd_usage)
+    sys.exit(1)
 
 arg = sys.argv[1].lower()
 
@@ -19,10 +22,12 @@ elif sys.argv[1] == "-j":
     POST_TWEET_LINK = "http://api.darrienglasser.com:8090/postjay"
     SECRET_KEY = "INSERT_JAY_KEY"
 elif sys.argv[1] == "-h":
-    print("Command usage\n\t-b \"<TWEET>\" to post to Bill\n\t-j \"<TWEET>\" to post to Jay\n\t-h for help")
-    os.exit(1)
+    print(cmd_usage)
+    sys.exit(1)
 else:
-    raise Exception("Invalid account to tweet to. Use -b for Bill or -j for Jay")
+    print("Invalid flag")
+    print(cmd_usage)
+    exit(1)
 
 tweet_data = {"secretKey":SECRET_KEY, "tweetText":sys.argv[2]}
 r = requests.post(POST_TWEET_LINK, data=tweet_data)
@@ -30,6 +35,7 @@ r = requests.post(POST_TWEET_LINK, data=tweet_data)
 if r.status_code != 202:
     print("Failed to post tweet 😢")
     print("Error code:" + str(r.status_code))
+    print("Response: " + r.text)
 else:
     print("TWEET POSTED 🙏")
     print(r.text)
